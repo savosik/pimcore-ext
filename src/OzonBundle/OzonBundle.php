61dd6286c99426b9f4997553a2de83aab67e744d
slava <slava.triput@gmail.com>
2021-09-11 22:28:19 +0300

    some changes

--- src/OzonBundle/OzonBundle.php
+++ src/OzonBundle/OzonBundle.php
@@ -1,10 +1,19 @@
 <?php
+declare(strict_types=1);
+
 namespace Savosik\OzonBundle;
 use Pimcore\Extension\Bundle\AbstractPimcoreBundle;
 use Pimcore\Extension\Bundle\Traits\PackageVersionTrait;
 
 
 class OzonBundle extends AbstractPimcoreBundle {
+    use PackageVersionTrait;
 
+    protected function getComposerPackageName(): string
+    {
+        // getVersion() will use this name to read the version from
+        // PackageVersions and return a normalized value
+        return 'savosik/pimcore-ext';
+    }
 
 }
