a22852dc0ff1f32a1f7b2a00287b6537a26e176c
triputvp <slava.triput@gmail.com>
2021-09-13 18:18:03 +0300

    Test settings DataObject store

--- src/OzonBundle/Command/OzonCategoriesUpdateCommand.php
+++ src/OzonBundle/Command/OzonCategoriesUpdateCommand.php
@@ -6,7 +6,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 
-use Pimcore\Model\DataObject\AppSettings;
+use \Pimcore\Model\DataObject;
 
 
 class OzonCategoriesUpdateCommand extends AbstractCommand{
@@ -22,12 +22,9 @@ class OzonCategoriesUpdateCommand extends AbstractCommand{
     {
 
 
-        $entries= new AppSettings\Listing();
+        $entries = new DataObject\AppSettings\Listing();
 
-
-        foreach ($entries as $entry){
-            var_dump($entry);
-        }
+        var_dump($entries);
 
         return 0;
     }
