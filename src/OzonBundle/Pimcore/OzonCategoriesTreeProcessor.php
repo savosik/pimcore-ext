c3c336027b358dc690ec3757606e215d0171a334
triputvp <slava.triput@gmail.com>
2021-09-16 13:09:51 +0300

    testing how shell script gets ozon categories

--- src/OzonBundle/Pimcore/OzonCategoriesTreeProcessor.php
+++ src/OzonBundle/Pimcore/OzonCategoriesTreeProcessor.php
@@ -7,7 +7,7 @@ class OzonCategoriesTreeProcessor{
 
     public function insertUpdate($ozon_categories_tree){
 
-        DataObject\Folder::create(['/test1','/test2','/test3']);
+        DataObject\Folder::create(['test1','test2','test3']);
     }
 
 
