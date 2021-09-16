c32d86613f5a7ea980b85e395c838fcb2443e806
triputvp <slava.triput@gmail.com>
2021-09-16 14:33:55 +0300

    testing how shell script gets ozon categories

--- src/OzonBundle/Pimcore/OzonCategoriesTreeProcessor.php
+++ src/OzonBundle/Pimcore/OzonCategoriesTreeProcessor.php
@@ -8,8 +8,10 @@ class OzonCategoriesTreeProcessor{
 
     public function insertUpdate($ozon_categories_tree){
 
-        DataObject\Service::createFolderByPath('/asd/arwe/wery');
+        $list = [];
+        $this->buildList($ozon_categories_tree,"/Ozon/Категории", $list);
 
+        var_dump($list);
     }
 
 
@@ -19,6 +21,25 @@ class OzonCategoriesTreeProcessor{
 
     public function createFolder(){
 
+    }
+
+
+
+    private function buildList($categories, $path = '', &$res = array()){
+        foreach ($categories as $category){
+            if(!empty($category['children'])){
+                $new_line = $path.$category['title']."/";
+                $this->buildList($category['children'], $new_line, $res);
+            }else{
+                echo $path.$category['title'].PHP_EOL;
+                $arr = [
+                    'category_path' => rtrim($path,'/'),
+                    'category_id' => $category['category_id'],
+                    'title' => $category['title']
+                ];
+                array_push($res, $arr);
+            }
+        }
     }
 
 }
