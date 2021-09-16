68c8771967b9d3aae7ed73af594b05992bdb586b
triputvp <slava.triput@gmail.com>
2021-09-16 14:54:13 +0300

    testing how shell script gets ozon categories

--- src/OzonBundle/Command/OzonCategoriesUpdateCommand.php
+++ src/OzonBundle/Command/OzonCategoriesUpdateCommand.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Savosik\OzonBundle\Command;
 
 use Pimcore\Console\AbstractCommand;
@@ -10,7 +11,8 @@ use Savosik\OzonBundle\Ozon\OzonDataProvider;
 use Savosik\OzonBundle\Pimcore\OzonCategoriesTreeProcessor;
 
 
-class OzonCategoriesUpdateCommand extends AbstractCommand{
+class OzonCategoriesUpdateCommand extends AbstractCommand
+{
 
     protected function configure()
     {
@@ -27,8 +29,9 @@ class OzonCategoriesUpdateCommand extends AbstractCommand{
         $ozon_client_id = $settings->getByKey('ozon_client_id');
         $ozon_token = $settings->getByKey('ozon_api_key');
         $ozon_parent_categories = $settings->getByKey('ozon_parent_categories');
+        $ozon_categories_start_path = $settings->getByKey('ozon_categories_pimcore_start_path');
 
-        $ozon_parent_categories  = explode(PHP_EOL, $ozon_parent_categories);
+        $ozon_parent_categories = explode(PHP_EOL, $ozon_parent_categories);
 
         $ozon_data_provider = new OzonDataProvider();
         $ozon_data_provider->setClientId($ozon_client_id);
@@ -36,10 +39,11 @@ class OzonCategoriesUpdateCommand extends AbstractCommand{
 
         $ozon_categories_processor = new OzonCategoriesTreeProcessor();
 
-        foreach ($ozon_parent_categories as $ozon_parent_category){
+
+        foreach ($ozon_parent_categories as $ozon_parent_category) {
 
             $categories = $ozon_data_provider->get_categories($ozon_parent_category);
-            $ozon_categories_processor->insertUpdate($categories);
+            $ozon_categories_processor->insertUpdate($categories, $ozon_categories_start_path);
         }
 
 
--- src/OzonBundle/Pimcore/OzonCategoriesTreeProcessor.php
+++ src/OzonBundle/Pimcore/OzonCategoriesTreeProcessor.php
@@ -1,39 +1,49 @@
 <?php
+
 namespace Savosik\OzonBundle\Pimcore;
 
 use Pimcore\Model\DataObject;
 
-class OzonCategoriesTreeProcessor{
+
+class OzonCategoriesTreeProcessor
+{
 
 
-    public function insertUpdate($ozon_categories_tree){
+    public function insertUpdate($ozon_categories_tree, $start_path = '')
+    {
 
-        $list = [];
-        $this->buildList($ozon_categories_tree,"/Ozon/Категории", $list);
+        $categories_list = [];
+        $this->buildList($ozon_categories_tree, $start_path, $categories_list);
 
-        var_dump($list);
+        foreach ($categories_list as $category){
+            DataObject\Service::createFolderByPath($category['category_path']);
+        }
     }
 
 
-    public function disableAllCategories(){
+    public function disableAllCategories()
+    {
 
     }
 
-    public function createFolder(){
+
+    public function createFolder()
+    {
 
     }
 
 
+    private function buildList($categories, $path = '', &$res = array())
+    {
 
-    private function buildList($categories, $path = '', &$res = array()){
-        foreach ($categories as $category){
-            if(!empty($category['children'])){
-                $new_line = $path.$category['title']."/";
+        foreach ($categories as $category) {
+
+            if (!empty($category['children'])) {
+                $new_line = $path . $category['title'] . "/";
                 $this->buildList($category['children'], $new_line, $res);
-            }else{
-                echo $path.$category['title'].PHP_EOL;
+            } else {
                 $arr = [
-                    'category_path' => rtrim($path,'/'),
+                    'category_path' => rtrim($path, '/'),
                     'category_id' => $category['category_id'],
                     'title' => $category['title']
                 ];
