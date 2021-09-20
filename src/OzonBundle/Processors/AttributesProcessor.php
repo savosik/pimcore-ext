32de48898d84f6dfab48fbcfcd7a6b50ac2c326f
triputvp <slava.triput@gmail.com>
2021-09-20 12:04:54 +0300

    Pushed refactored Classes

--- src/OzonBundle/Processors/AttributesProcessor.php
+++ src/OzonBundle/Processors/AttributesProcessor.php
@@ -2,21 +2,24 @@
 namespace Savosik\OzonBundle\Processors;
 
 use Pimcore\Model\DataObject;
+use Pimcore\Model\Element;
 
 
 class AttributesProcessor{
 
     public function insertUpdate($ozon_attributes, $path = ''){
 
+        $folder = DataObject\Folder::getByPath($path);
+        $obj_parent_id = $folder->getId();
+
+
         $ozon_attributes = $ozon_attributes[0]['attributes'];
 
         foreach ($ozon_attributes as $ozon_attribute){
 
-            var_dump($ozon_attribute);
-
             $new_obj = new DataObject\OzonAttribute();
 
-            $new_obj->setKey($ozon_attribute['name']);
+            $new_obj->setKey(Element\Service::getValidKey($ozon_attribute['name'], 'object'));
             $new_obj->setPublished(true);
             $new_obj->setAttr_id($ozon_attribute['id']);
             $new_obj->setName($ozon_attribute['name']);
@@ -28,7 +31,7 @@ class AttributesProcessor{
             $new_obj->setGroup_name($ozon_attribute['group_name']);
             $new_obj->setDictionary_id($ozon_attribute['dictionary_id']);
 
-            $new_obj->setPath($path);
+            $new_obj->setParentId($obj_parent_id);
 
             $new_obj->save();
         }
